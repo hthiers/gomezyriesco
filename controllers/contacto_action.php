c06f78a115d3306e1cc67473fd83ce56fc1c558d
Hernan Thiers <hernanthiers@gmail.com>
2016-11-08 02:04:58 -0300

    ajustes menores y formulario contacto accion.

--- controllers/contacto_action.php
+++ controllers/contacto_action.php
@@ -8,5 +8,21 @@ $from = 'contacto@gomezyriesco.cl';
 #$to = 'secretaria@gomezyriesco.cl'; 
 $to = 'hernanthiers@gmail.com'; 
 $subject = 'Contacto desde sitio web gomezyriesco.cl';
- 
+
 $body = "From: $nombre\n E-Mail: $email\n Telefono: $fono\n Mensaje:\n $comentarios";
+
+if (mail ($to, $subject, $body, $from)) {
+    // Set a 200 (okay) response code.
+    http_response_code(200);
+    echo "ok";
+
+    #$result=true;
+} else {
+    // Set a 500 (internal server error) response code.
+    http_response_code(500);
+    echo "error";
+
+    #$result=false;
+}
+
+return $result;
